0ce3d7ebd3a4e671b4c4e00f2f3b6679c0bc941f
shyuni4u <shyuniz@naver.com>
2025-07-12 19:10:53 +0900

    Add google analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,13 @@ import { pretendard } from '@/lib/fonts/pretendard'
 import './globals.css'
 import { cn } from '@/lib/utils'
 import { ThemeProvider } from 'next-themes'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ko" suppressHydrationWarning className={cn(pretendard.className, 'scroll-smooth')}>
       <body>
+        <GoogleAnalytics gaId="G-JJZRZVQ1NK" />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
