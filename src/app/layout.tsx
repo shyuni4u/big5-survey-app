caf16b4c412aa456b5c423c9e4e24c074edf899b
SunwooKim <shyuniz@naver.com>
2025-08-02 12:22:08 +0900

    Remove adsense. (#15)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,14 +7,6 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ko" suppressHydrationWarning className={cn(pretendard.className, 'scroll-smooth')}>
-      <head>
-        <meta name="google-adsense-account" content="ca-pub-7797435335029081" />
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7797435335029081"
-          crossOrigin="anonymous"
-        ></script>
-      </head>
       <body>
         <GoogleAnalytics gaId="G-2FW5XLPK5V" />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
